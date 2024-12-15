feec77cf77fdb10f5ef95e953d704ed8343eb5f0
lrecine- <lucasrecine@gmail.com>
2024-12-15 20:13:06 -0300

    minitalk

---
+++ src/server.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrecine- <lrecine-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 19:57:49 by lrecine-          #+#    #+#             */
+/*   Updated: 2024/12/15 19:57:55 by lrecine-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	while (argc == 1)
+	{
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+		pause ();
+	}
+	return (0);
+}
---
+++ src/server_bonus.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrecine- <lrecine-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 19:57:19 by lrecine-          #+#    #+#             */
+/*   Updated: 2024/12/15 19:57:44 by lrecine-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+void	ft_btoa(int sig, siginfo_t *info, void *context)
+{
+	static int	bit;
+	static int	i;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		if (i == 0)
+			kill(info->si_pid, SIGUSR2);
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int					pid;
+	struct sigaction	act;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	act.sa_sigaction = ft_btoa;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	while (argc == 1)
+	{
+		sigaction(SIGUSR1, &act, NULL);
+		sigaction(SIGUSR2, &act, NULL);
+		pause();
+	}
+	return (0);
+}
